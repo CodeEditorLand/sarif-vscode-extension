00460da8576e4f809398473535334a48a5d98597
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-27 06:56:21 +0300

    squash!
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- src/panel/filterKeywordContext.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-import { createContext } from 'react';
-
-export const FilterKeywordContext = createContext('');
